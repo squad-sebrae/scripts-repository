30cd805a5d2a4b444c5a702cca0a0b0941ac92e7
Diego Choli <dconceicao@pr.sebrae.com.br>
2023-08-11 10:46:55 -0300

    Finalizado scripot de integração bcs

--- integracoes/bcs-integration.js
+++ integracoes/bcs-integration.js
@@ -1,18 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
   const url = window.location.pathname
 
-  // Divide a URL em partes pelo caractere '/'
-  const partes = url.split('/')
-
-  // Obtém o último item do array partes (nome da página)
-  let pagina = partes.pop()
-  if (!pagina) pagina = partes.pop()
-
-  if (pagina === 'feiradoempreendedor') {
-    console.log('==========================================')
-    console.log(pagina) // imprime o nome da página atual, sem a extensão
-    console.log('==========================================')
-
+  if (url.indexOf('feiradoempreendedor') !== -1) {
     document
       .getElementById('SubmitForm')
       .addEventListener('submit', (event) => {
@@ -69,16 +58,17 @@ window.addEventListener('DOMContentLoaded', function () {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
+              Origin: 'https://www.sebraepr.com.br',
             },
             body: JSON.stringify(body),
           }
 
-          console.log(body)
-
           fetch(urlBCS, options)
             .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.log(error))
+            .then((data) => console.log('Integração com a BCS concluída'))
+            .catch((error) =>
+              console.log("Erro na integração com a BCS concluída'")
+            )
         } catch {
           console.log('Erro ao integrar com a BCS')
         }
